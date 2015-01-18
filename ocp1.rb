aee1585a2cf608297548f89553404fa3ea4678b0
Vinícius Oyama <vinicius.oyama@codus.com.br>
2015-01-18 16:30:22 -0200

    finished ocp1

--- ocp1.rb
+++ ocp1.rb
@@ -47,3 +47,14 @@ class UpdateStoreProduct
     scraper.get_prices(@store_product.url)
   end
 end
+
+# with OCP
+
+class UpdateStoreProduct
+
+  def get_prices
+    scraper_detalhes = Scrapers.const_get(@store_product.store_name).new
+    scraper.get_prices(@store_product.url)
+  end
+
+end
